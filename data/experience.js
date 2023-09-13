9073ed2216c1b25d967283b344c0df72fe06ad7d
Shrey Bhadiyadara <shreybhadiyadara33@yahoo.com>
2023-09-13 01:50:26 -0700

    Pdf update

--- data/experience.js
+++ data/experience.js
@@ -3,12 +3,12 @@ export const EXPERIENCE = [
         companyName: "Neliti Pte. Ltd.",
         companyDescription: [
             "Leveraged Django, Docker, Celery, and Postgres and utilized HTML, CSS, Sass, and JavaScript to develop responsive a web application with 12+ million monthly user traffic",
-            "Developed an internal page builder that significantly reduced average developer time by 70% by providing readily available static pages and also developed Neliti Advertisement platform leveraging Compact Serving method for ads allocation",
+            "Developed an internal page builder that significantly reduced average developer time by 70% by providing readily available static pages and also developed the Neliti Advertisement platform leveraging the Compact Serving method for ad allocation",
             "Implemented AlpineJS to improve code readability and UI reactivity by 20%, reducing code duplication by 11%",
             "Leveraged tools/technologies like Sentry, Unsplash API, Solr, Docker, and Webflow to enhance application functionalities like optimizing Solr schema for faster search results by 15% and implementing viewport-based image loading",
             "Enhanced UI/UX, SEO, accessibility, and performance metrics by leveraging HTML, Sass, CSS, and JavaScript, substantially improving Lighthouse scores by 100%",
             "Designed and deployed an internal dashboard application using Django Rest Framework, React, Redux, Tailwind CSS, Vite, Jest, and AWS, implementing GitHub Actions CI/CD, saving over 30 minutes of manual deployment time",
-            "Engineered a dynamic viewport-based text calculator implementing binary search algorithm for generating visually appealing site headers of more than 4.5k journals, conferences and repositories sites",
+            "Engineered a dynamic viewport-based text calculator implementing a binary search algorithm for generating visually appealing site headers of more than 4.5k journal, conference and repository sites",
             "Optimized page loading speed by utilizing Memcached and implementing JavaScript asynchronous execution, resulting in a remarkable 25% improvement in overall website performance"
         ],
         companyLink: "https://www.neliti.com/",
@@ -32,7 +32,7 @@ export const EXPERIENCE = [
             "Led the migration of TallyWiz, a former brand, to the Suvit platform, ensuring uninterrupted user service",
             "Implemented real-time functionality using Socket.io for seamless updates within the application to meet customer demands, saving over 20% API calls to the server and client-side refreshes",
             "Collaborated with cross-functional team members to design and implement robust auth and user profile management with Redux and MongoDB database, allowing for login functionality and user onboarding with optimized click-through rate",
-            "Optimized application performance by reducing the average loading time, from over 2 seconds to below 0.8 seconds by memoizing function calls, reduced API payload, and React best practices, when rendering a large dataset of 10,000 rows",
+            "Optimized application performance by reducing the average loading time, from over 2 seconds to below 0.8 seconds when rendering a large dataset of 10,000 rows by memoizing function calls, reduced API payload, and React best practices",
             "Integrated D3.js for data visualization of 10,000 rows and utilized Intercom for efficient customer query resolution, resulting in enhanced data presentation and a streamlined customer support process"
         ],
         companyLink: "https://www.suvit.io/",
