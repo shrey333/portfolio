10952c7894e0f1340065e24a7a4bd97de58d2242
shrey <shreybhadiyadara33@gmail.com>
2025-05-08 16:19:47 -0500

    style: fix background styling and add horizontal overflow prevention

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head></Head>
-        <body className="bg-white dark:bg-black text-dark dark:text-white absolute inset-0 -z-10 size-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] h-screen w-screen dark:bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] dark:bg-[size:20px_20px]">
+        <body className="bg-white dark:bg-black text-dark dark:text-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] dark:bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] dark:bg-[size:20px_20px] overflow-x-hidden">
           <Main />
           <NextScript />
         </body>
